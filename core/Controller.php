e8dc2efa8aaa67e64bd76a4856227647b945e3ee
Bat Paulin <batpaulin@gmail.com>
2018-05-07 01:46:54 +0200

    Mise en place d'extensions Twig

--- core/Controller.php
+++ core/Controller.php
@@ -1,9 +1,8 @@
 <?php
 namespace niluap\core;
 
-use Twig\TwigFilter;
-use Twig\TwigFunction;
 use Twig_Environment;
+use Twig_Extensions_Extension_Text;
 use Twig_Loader_Filesystem;
 
 class Controller {
@@ -20,11 +19,8 @@ class Controller {
         $this->twig = new Twig_Environment($loader, [
             'cache' => false
         ]);
-        $this->twig->addFilter($this->twigGetUrl());
-        $this->twig->addFilter($this->twigGetActiveMenu());
-        $this->twig->addFilter($this->twigGetExcerpt());
-        $this->twig->addFunction($this->twigGetAlert());
-        $this->twig->addGlobal('session', $_SESSION);
+        $this->twig->addExtension(new Twig_Extensions_Extension_Text());
+        $this->twig->addExtension(new MyTwigExtension());
     }
 
     /**
@@ -34,45 +30,6 @@ class Controller {
      */
     public function render(string $view, $variables = []) {
         echo $this->twig->render($view, $variables);
-    }
-
-    /**
-     * @param string $excerpt
-     * @param int $length
-     * @param bool $dot
-     * @return bool|string
-     * Make an excerpt from a text (comment, post, ...)
-     */
-    public static function getExcerpt(string $excerpt, int $length, bool $dot = true) {
-        if ($dot == false) {
-            return substr($excerpt, 0, $length);
-        } else {
-            return substr($excerpt, 0, $length) . '...';
-        }
-    }
-
-    public function twigGetExcerpt() {
-        return $newFilter = new TwigFilter('excerpt', function (string $excerpt, int $length, bool $dot = true) {
-            return self::getExcerpt($excerpt, $length, $dot);
-        });
-    }
-
-    public function twigGetAlert() {
-        return $newFunction = new TwigFunction('getAlert', function () {
-            Alert::getAlert();
-        });
-    }
-
-    public function twigGetUrl() {
-        return $newFilter = new TwigFilter('getUrl', function ($route) {
-            return Router::getUrl($route);
-        });
-    }
-
-    public function  twigGetActiveMenu() {
-        return $newFilter = new TwigFilter('getActiveMenu', function ($url) {
-            return Router::getActiveMenu($url);
-        });
     }
 
 }
---
+++ core/MyTwigExtension.php
@@ -0,0 +1,42 @@
+<?php
+namespace niluap\core;
+
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig_Extension;
+use Twig_Extension_GlobalsInterface;
+
+class MyTwigExtension extends Twig_Extension implements Twig_Extension_GlobalsInterface {
+
+    public function getFilters() {
+        return [
+            new TwigFilter('getUrl', [$this, 'twigGetUrl']),
+            new TwigFilter('getActiveMenu', [$this, 'twigGetActiveMenu'])
+        ];
+    }
+
+    public function getFunctions() {
+        return [
+            new TwigFunction('getAlert', [$this, 'twigGetAlert'])
+        ];
+    }
+
+    public function getGlobals() {
+        return [
+            'session' => $_SESSION
+        ];
+    }
+
+    public function twigGetAlert() {
+        Alert::getAlert();
+    }
+
+    public function twigGetUrl($route) {
+        return Router::getUrl($route);
+    }
+
+    public function twigGetActiveMenu($url) {
+        return Router::getActiveMenu($url);
+    }
+
+}
