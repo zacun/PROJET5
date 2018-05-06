9d44137bedf71d57963c01f7780be0b26d3daaf9
Bat Paulin <batpaulin@gmail.com>
2018-05-06 02:25:14 +0200

    Mise en place de Twig

--- core/Controller.php
+++ core/Controller.php
@@ -1,10 +1,28 @@
 <?php
 namespace niluap\core;
 
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
 class Controller {
 
-    protected $viewPath = '../views/';
-    protected $template = 'default';
+    protected $viewsPaths = [
+        'public' => __DIR__ . '/../views/public',
+        'admin' => __DIR__ . '/../views/admin',
+        'template' => __DIR__ . '/../views/template'
+    ];
+    private $twig;
+
+    public function __construct() {
+        $loader = new Twig_Loader_Filesystem($this->viewsPaths);
+        $this->twig = new Twig_Environment($loader, []);
+        $this->twig->addFilter($this->twigGetUrl());
+        $this->twig->addFilter($this->twigGetActiveMenu());
+        $this->twig->addFilter($this->twigGetExcerpt());
+        $this->twig->addFunction($this->twigGetAlert());
+    }
 
     /**
      * @param string $view
@@ -12,12 +30,7 @@ class Controller {
      * render the right view with all needed variables.
      */
     public function render(string $view, $variables = []) {
-        extract($variables);
-        ob_start();
-        $title = '';
-        require($this->viewPath . $view .'.php');
-        $content = ob_get_clean();
-        require($this->viewPath . 'template/' . $this->template . '.php');
+        echo $this->twig->render($view, $variables);
     }
 
     /**
@@ -33,6 +46,30 @@ class Controller {
         } else {
             return substr($excerpt, 0, $length) . '...';
         }
+    }
+
+    public function twigGetExcerpt() {
+        return $newFilter = new TwigFilter('excerpt', function (string $excerpt, int $length, bool $dot = true) {
+            return self::getExcerpt($excerpt, $length, $dot);
+        });
+    }
+
+    public function twigGetAlert() {
+        return $newFunction = new TwigFunction('getAlert', function () {
+            Alert::getAlert();
+        });
+    }
+
+    public function twigGetUrl() {
+        return $newFilter = new TwigFilter('getUrl', function ($route) {
+            return Router::getUrl($route);
+        });
+    }
+
+    public function  twigGetActiveMenu() {
+        return $newFilter = new TwigFilter('getActiveMenu', function ($url) {
+            return Router::getActiveMenu($url);
+        });
     }
 
 }
