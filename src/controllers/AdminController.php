8277f64e021aa1ae1c5255cfc7abaa85caa47c29
Bat Paulin <batpaulin@gmail.com>
2018-05-07 17:23:23 +0200

    Ajout de la page admin

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -2,6 +2,8 @@
 namespace niluap\src\controllers;
 
 use niluap\core\Controller;
+use niluap\src\models\PostsManager;
+use niluap\src\models\ProjectsManager;
 
 /**
  * Class AdminController
@@ -9,5 +11,13 @@ use niluap\core\Controller;
  * This class contains all function related to the admins such as displaying admin pages or allowing admin actions (delete comments/posts, add post/project, ...)
  */
 class AdminController extends Controller {
+
+    public function adminPage () {
+        $projectsManager = new ProjectsManager();
+        $allProjects = $projectsManager->getAllProjects();
+        $postsManager = new PostsManager();
+        $allPosts = $postsManager->getAllPosts();
+        $this->render('admin.twig', compact('allProjects', 'allPosts'));
+    }
 
 }
